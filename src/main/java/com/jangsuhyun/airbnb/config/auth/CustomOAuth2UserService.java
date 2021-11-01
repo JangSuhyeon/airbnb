4517158084ea9b3e4d83aaeec210d323ea237921
JangSuhyeon <shjang0043@gmail.com>
2021-11-01 14:16:24 +0900

    OAUTH2 소셜 로그인 기능

---
+++ src/main/java/com/jangsuhyun/airbnb/config/auth/CustomOAuth2UserService.java
@@ -0,0 +1,67 @@
+package com.jangsuhyun.airbnb.config.auth;
+
+import com.jangsuhyun.airbnb.domain.user.User;
+import com.jangsuhyun.airbnb.domain.user.UserRepository;
+import com.jangsuhyun.airbnb.config.auth.dto.OAuthAttributes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.lang.reflect.Array;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService<OAuth2UserRequest,OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+        User user = saveOrUpdate(attributes);
+
+        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey());
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
+
+        //회원가입을 해야 Cart Entity에 userId를 넣을 수 있어서 먼저 User save
+        User returnUser = userRepository.save(user);
+
+        return returnUser;
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    public Boolean emptyCheck(Object obj) {
+        if (obj instanceof String) return obj == null || "".equals(obj.toString().trim());
+        else if (obj instanceof List) return obj == null || ((List) obj).isEmpty();
+        else if (obj instanceof Map) return obj == null || ((Map) obj).isEmpty();
+        else if (obj instanceof Object[]) return obj == null || Array.getLength(obj) == 0;
+        else return obj == null;
+    }
+}
